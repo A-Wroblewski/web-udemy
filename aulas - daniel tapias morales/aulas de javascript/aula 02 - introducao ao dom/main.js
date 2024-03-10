5b8474defaa80b82f2b8177bd3b25c71d2eb1915
A-Wroblewski <62357109+A-Wroblewski@users.noreply.github.com>
2024-03-10 09:21:12 -0300

    mais dom e projeto 1

--- aulas - daniel tapias morales/aulas de javascript/aula 02 - introducao ao dom/main.js
+++ aulas - daniel tapias morales/aulas de javascript/aula 02 - introducao ao dom/main.js
@@ -22,3 +22,30 @@ let list = document.getElementById('list');
 let newFruit = '<li>Durião</li>';
 
 list.innerHTML += newFruit;
+
+const userName = null;
+const userNameParagraph = document.getElementById('username-paragraph');
+
+if (userName) {
+    userNameParagraph.innerHTML += `Boas-vindas, ${userName}`;
+}
+
+const textInput = document.getElementById('text-input');
+
+function editText() {
+    textInput.removeAttribute('disabled');
+    textInput.focus();
+}
+
+function setDisabled() {
+    textInput.toggleAttribute('disabled');
+}
+
+function acceptTerms() {
+    const checkBox = document.getElementById('agreement');
+    const signUpButton = document.getElementById('sign-up-button');
+
+    let isChecked = checkBox.checked;
+
+    signUpButton.disabled = !isChecked;
+}
